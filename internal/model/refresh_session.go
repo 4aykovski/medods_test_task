8f047723835201ac57b912c026c5f01298649da4
4aykovski <kan89kan@gmail.com>
2024-02-26 18:12:48 +0300

    fix: remove array from user model and add user id to refresh_session model

--- internal/model/refresh_session.go
+++ internal/model/refresh_session.go
@@ -4,6 +4,7 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type RefreshSession struct {
 	ID           primitive.ObjectID `bson:"_id"`
+	GUID         string             `bson:"guid"`
 	RefreshToken string             `bson:"refresh_token"`
 	ExpiresIn    primitive.DateTime `bson:"expires_in"`
 }
--- internal/model/user.go
+++ internal/model/user.go
@@ -3,8 +3,6 @@ package model
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type User struct {
-	ID                 primitive.ObjectID   `bson:"_id"`
-	GUID               string               `bson:"guid"`
-	Password           string               `bson:"password"`
-	RefreshSessionsIDs []primitive.ObjectID `bson:"refresh_sessions_ids"`
+	ID   primitive.ObjectID `bson:"_id"`
+	GUID string             `bson:"guid"`
 }
