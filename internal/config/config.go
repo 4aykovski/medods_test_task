c88bedd10462f44174fdd32604fd510114a6bbd1
4aykovski <kan89kan@gmail.com>
2024-02-26 16:05:18 +0300

    fix: change config structure

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,35 @@
 package config
+
+import "fmt"
+
+type Config struct {
+	Secret     string
+	HTTPServer HTTPServer
+	Mongodb    Mongodb
+}
+
+type HTTPServer struct {
+	Address string
+}
+
+type Mongodb struct {
+	Host string
+	Port int
+	URI  string
+}
+
+func MustLoad() *Config {
+	// из-за ограниченного стека используемых технологий не использую godotenv и cleanenv для заполнения конфига
+	cfg := &Config{
+		Secret:     "get_secret_from_env",
+		HTTPServer: HTTPServer{Address: "localhost:8080"},
+		Mongodb: Mongodb{
+			Host: "localhost",
+			Port: 27017,
+		},
+	}
+
+	cfg.Mongodb.URI = fmt.Sprintf("mongodb://%s:%s", cfg.Mongodb.Host, cfg.Mongodb.Port)
+
+	return cfg
+}
