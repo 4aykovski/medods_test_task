590738274df2a48a8c9bfe4ac204bca77382f459
4aykovski <kan89kan@gmail.com>
2024-02-26 16:16:04 +0300

    feat: repository errors = ErrSessionAlreadyExists and ErrSessionNotFound

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -3,6 +3,8 @@ package repository
 import "errors"
 
 var (
-	ErrUserAlreadyExists = errors.New("user already exists")
-	ErrUserNotFound      = errors.New("user not found")
+	ErrUserAlreadyExists    = errors.New("user already exists")
+	ErrUserNotFound         = errors.New("user not found")
+	ErrSessionAlreadyExists = errors.New("refresh session already exists")
+	ErrSessionNotFound      = errors.New("refresh session not found")
 )
