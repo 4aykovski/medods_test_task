d12a60137640fc57e4d7b34e41794438e910cb97
4aykovski <kan89kan@gmail.com>
2024-02-26 16:05:59 +0300

    feat: userRepository - insert and find methods

--- internal/repository/mongo/user.go
+++ internal/repository/mongo/user.go
@@ -1,1 +1,58 @@
 package mongo
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/4aykovksi/medods_test_task/internal/model"
+	"github.com/4aykovksi/medods_test_task/internal/repository"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type UsersRepository struct {
+	db *mongo.Collection
+}
+
+func NewUserRepository(db *mongo.Database) *UsersRepository {
+	return &UsersRepository{
+		db: db.Collection(usersCollection),
+	}
+}
+
+func (repo *UsersRepository) Insert(ctx context.Context, user *model.User) error {
+	const op = "internal.repository.mongo.user.Insert"
+
+	_, err := repo.db.InsertOne(ctx, user)
+	if err != nil {
+		var mongoErr mongo.WriteError
+		if errors.As(err, &mongoErr) {
+			if mongoErr.Code == 11000 {
+				return repository.ErrUserAlreadyExists
+			}
+		}
+
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	return nil
+}
+
+func (repo *UsersRepository) FindByGUID(ctx context.Context, guid string) (*model.User, error) {
+	const op = "internal.repository.mongo.user.FindByGUID"
+
+	filter := bson.D{{"guid", guid}}
+
+	var user model.User
+	err := repo.db.FindOne(ctx, filter).Decode(&user)
+	if err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			return nil, repository.ErrUserNotFound
+		}
+
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return &user, nil
+}
