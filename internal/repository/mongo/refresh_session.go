9fc66890ca617b2c860c93be9d3db0a2debd876a
4aykovski <kan89kan@gmail.com>
2024-02-26 16:18:03 +0300

    feat: refreshSessionRepository - Insert, FindByToken, DeleteByToken methods

---
+++ internal/repository/mongo/refresh_session.go
@@ -0,0 +1,74 @@
+package mongo
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/4aykovksi/medods_test_task/internal/model"
+	"github.com/4aykovksi/medods_test_task/internal/repository"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type RefreshSessionRepository struct {
+	db *mongo.Collection
+}
+
+func NewRefreshSessionsRepository(db *mongo.Database) *RefreshSessionRepository {
+	return &RefreshSessionRepository{
+		db: db.Collection(refreshSessionCollection),
+	}
+}
+
+func (repo *RefreshSessionRepository) Insert(ctx context.Context, session model.RefreshSession) error {
+	const op = "internal.repository.mongo.refresh_session.Insert"
+
+	_, err := repo.db.InsertOne(ctx, session)
+	if err != nil {
+		var mongoErr mongo.WriteError
+		if errors.As(err, &mongoErr) {
+			if mongoErr.Code == 11000 {
+				return repository.ErrSessionAlreadyExists
+			}
+		}
+
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	return nil
+}
+
+func (repo *RefreshSessionRepository) FindByToken(ctx context.Context, token string) (*model.RefreshSession, error) {
+	const op = "internal.repository.mongo.refresh_session.FindByToken"
+
+	filter := bson.D{{"refresh_token", token}}
+
+	var session model.RefreshSession
+	err := repo.db.FindOne(ctx, filter).Decode(&session)
+	if err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			return nil, repository.ErrSessionNotFound
+		}
+
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return &session, nil
+}
+
+func (repo *RefreshSessionRepository) DeleteByToken(ctx context.Context, token string) error {
+	const op = "internal.repository.mongo.refresh_session.DeleteByToken"
+
+	filter := bson.D{{"refresh_token", token}}
+
+	result, err := repo.db.DeleteOne(ctx, filter)
+	if result.DeletedCount == 0 {
+		return repository.ErrSessionNotFound
+	}
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	return nil
+}
