4bd645fd0dc3539b1ad5fbec2cd38d1a8d159fa2
4aykovski <kan89kan@gmail.com>
2024-02-27 13:20:01 +0700

    feat: add server init

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"log/slog"
+	"net/http"
 	"os"
 
 	"github.com/4aykovksi/medods_test_task/internal/config"
 	"github.com/4aykovksi/medods_test_task/internal/repository/mongorepos"
+	v1 "github.com/4aykovksi/medods_test_task/internal/rest/v1"
 	"github.com/4aykovksi/medods_test_task/internal/services"
 	"github.com/4aykovksi/medods_test_task/pkg/database/mongodb"
 	"github.com/4aykovksi/medods_test_task/pkg/lib/auth"
@@ -38,9 +40,22 @@ func main() {
 	bcryptHasher := hasher.NewBcryptHasher()
 	tokenManager := auth.NewManager(cfg.Secret)
 
-	sessionService := services.NewRefreshSessionService(sessionRepo, bcryptHasher)
-	userService := services.NewUserService(userRepo, sessionService, tokenManager, bcryptHasher, cfg.AccessTokenTTL, cfg.RefreshTokenTTL)
+	sessionService := services.NewRefreshSessionService(sessionRepo, bcryptHasher, 3)
+	authService := services.NewAuthService(userRepo, sessionService, tokenManager, bcryptHasher, cfg.AccessTokenTTL, cfg.RefreshTokenTTL)
 
-	// TODO: init router
-	// TODO: run server
+	// init router
+	r := v1.NewRouter(log, authService)
+
+	// run server
+	log.Info("server started", slog.String("address", cfg.HTTPServer.Address))
+
+	server := http.Server{
+		Addr:    cfg.HTTPServer.Address,
+		Handler: r,
+	}
+	err = server.ListenAndServe()
+	if err != nil {
+		log.Error("server stopped", slog.String("err", err.Error()))
+		os.Exit(1)
+	}
 }
