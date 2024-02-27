6fd69e13d9c19e3704269014f4226b952720ffee
4aykovski <kan89kan@gmail.com>
2024-02-27 16:30:29 +0700

    feat: add docstrings to handlers and create refresh handler

--- internal/rest/v1/handler/auth.go
+++ internal/rest/v1/handler/auth.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
+	"io"
 	"log/slog"
 	"net/http"
 	"time"
@@ -15,7 +16,8 @@ import (
 )
 
 const (
-	TokenNotSpecifiedMsg   = "token wasn't specified"
+	GuidNotSpecifiedMsg    = "guid wasn't specified"
+	TokenNotSpecifiedMsg   = "refresh token is not specified"
 	WrongCredentialsMsg    = "wrong credentials"
 	InternalServerErrorMsg = "internal server error"
 	refreshCookieName      = "refreshToken"
@@ -24,6 +26,7 @@ const (
 
 type authService interface {
 	SignIn(ctx context.Context, input services.AuthSignInInput) (*auth.Tokens, error)
+	Refresh(ctx context.Context, base64token string) (*auth.Tokens, error)
 }
 
 type AuthHandler struct {
@@ -40,14 +43,25 @@ func NewAuthHandler(
 
 type authSignInOutput struct {
 	response.Response
-	AccessToken  string
-	RefreshToken string
+	AccessToken  string `json:"access_token,omitempty"`
+	RefreshToken string `json:"refresh_token,omitempty"`
 }
 
+// SignIn handles sign in requests
+// 200 - OK. response contains access and refresh tokens, refresh cookies is set
+// 400 - guid is not specified.
+// 407 - can't find given guid in database
+// 500 - various internal server errors
 func (h *AuthHandler) SignIn(log *slog.Logger) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		const op = "rest.v1.handler.auth.SignIn"
+
 		// TODO: add middleware to logging request processing
+		log = log.With(slog.String("op", op))
 		log.Info("start processing request")
+
+		deleteCookie := h.newRefreshCookie("", time.Now())
+		http.SetCookie(w, deleteCookie)
 
 		w.Header().Set("Content-Type", "application/json")
 
@@ -55,16 +69,7 @@ func (h *AuthHandler) SignIn(log *slog.Logger) http.HandlerFunc {
 		if guid == "" {
 			log.Info("token wasn't specified")
 
-			w.WriteHeader(http.StatusBadRequest)
-			res := response.Error(TokenNotSpecifiedMsg)
-			jsonRes, err := json.Marshal(res)
-			if err != nil {
-				log.Error("internal error on marshalling response", slog.String("err", err.Error()))
-
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-			w.Write(jsonRes)
+			sendErrorResponse(log, w, GuidNotSpecifiedMsg, http.StatusBadRequest)
 			return
 		}
 
@@ -73,29 +78,13 @@ func (h *AuthHandler) SignIn(log *slog.Logger) http.HandlerFunc {
 			if errors.Is(err, repository.ErrUserNotFound) {
 				log.Info("wrong credentials")
 
-				w.WriteHeader(http.StatusProxyAuthRequired)
-				res := response.Error(WrongCredentialsMsg)
-				jsonRes, err := json.Marshal(res)
-				if err != nil {
-					log.Error("internal error on marshalling response", slog.String("err", err.Error()))
-
-					w.WriteHeader(http.StatusInternalServerError)
-					return
-				}
-				w.Write(jsonRes)
+				sendErrorResponse(log, w, WrongCredentialsMsg, http.StatusProxyAuthRequired)
 				return
 			}
 
 			log.Error("internal error on authService.SignIn", slog.String("err", err.Error()))
 
-			w.WriteHeader(http.StatusInternalServerError)
-			res := response.Error(InternalServerErrorMsg)
-			jsonRes, err := json.Marshal(res)
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-			w.Write(jsonRes)
+			sendErrorResponse(log, w, InternalServerErrorMsg, http.StatusInternalServerError)
 			return
 		}
 
@@ -119,8 +108,84 @@ func (h *AuthHandler) SignIn(log *slog.Logger) http.HandlerFunc {
 	}
 }
 
+type authRefreshInput struct {
+	RefreshToken string `json:"refresh_token,omitempty"`
+}
+
+type authRefreshOutput struct {
+	response.Response
+	AccessToken  string `json:"access_token,omitempty"`
+	RefreshToken string `json:"refresh_token,omitempty"`
+}
+
+// Refresh handles refresh request
+// 200 - OK. response contains access and refresh tokens, refresh cookies is set
+// 400 - refresh token is not specified.
+// 407 - refresh token wasn't find or it's not valid
+// 500 - various internal server errors
 func (h *AuthHandler) Refresh(log *slog.Logger) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		const op = "rest.v1.handler.auth.Refresh"
+
+		// TODO: add middleware to logging request processing
+		log = log.With(slog.String("op", op))
+		log.Info("start processing request")
+
+		w.Header().Set("Content-Type", "application/json")
+
+		var token string
+		cookie, err := r.Cookie(refreshCookieName)
+		if err != nil {
+			var req authRefreshInput
+			body, err := io.ReadAll(r.Body)
+			if err != nil {
+				log.Error("can't decode request body", slog.String("err", err.Error()))
+
+				sendErrorResponse(log, w, InternalServerErrorMsg, http.StatusInternalServerError)
+				return
+			}
+			err = json.Unmarshal(body, &req)
+			if err != nil || req.RefreshToken == "" {
+				log.Info("refresh_cookie is not specified")
+
+				sendErrorResponse(log, w, TokenNotSpecifiedMsg, http.StatusBadRequest)
+				return
+			}
+			token = req.RefreshToken
+		} else {
+			token = cookie.Value
+		}
+
+		tokens, err := h.authService.Refresh(r.Context(), token)
+		if err != nil {
+			if errors.Is(err, services.ErrWrongCred) {
+				log.Info("wrong credentials")
+
+				sendErrorResponse(log, w, WrongCredentialsMsg, http.StatusProxyAuthRequired)
+				return
+			}
+			log.Error("internal server error", slog.String("err", err.Error()))
+
+			sendErrorResponse(log, w, InternalServerErrorMsg, http.StatusInternalServerError)
+			return
+		}
+
+		refreshCookie := h.newRefreshCookie(tokens.RefreshToken, tokens.ExpiresIn)
+		http.SetCookie(w, refreshCookie)
+
+		log.Info("successfully signed in")
+
+		res := authRefreshOutput{
+			Response:     response.OK(),
+			AccessToken:  tokens.AccessToken,
+			RefreshToken: tokens.RefreshToken,
+		}
+		jsonRes, err := json.Marshal(res)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Write(jsonRes)
 		return
 	}
 
@@ -136,4 +201,19 @@ func (h *AuthHandler) newRefreshCookie(refreshToken string, time time.Time) *htt
 		HttpOnly: true,
 		SameSite: 3,
 	}
+}
+
+// sendErrorResponse send response with given statusCode as http status code and msg in body
+func sendErrorResponse(log *slog.Logger, w http.ResponseWriter, msg string, statusCode int) {
+	w.WriteHeader(statusCode)
+	res := response.Error(msg)
+	jsonRes, err := json.Marshal(res)
+	if err != nil {
+		log.Error("internal error on marshalling response", slog.String("err", err.Error()))
+
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Write(jsonRes)
+	return
 }
