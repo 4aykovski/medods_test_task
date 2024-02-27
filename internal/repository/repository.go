d71de4b384ada9fa87368c774efa97e051cb3215
4aykovski <kan89kan@gmail.com>
2024-02-27 18:30:38 +0300

    fix: delete unused error

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -3,7 +3,6 @@ package repository
 import "errors"
 
 var (
-	ErrUserAlreadyExists    = errors.New("user already exists")
 	ErrUserNotFound         = errors.New("user not found")
 	ErrSessionAlreadyExists = errors.New("refresh session already exists")
 	ErrSessionNotFound      = errors.New("refresh session not found")
