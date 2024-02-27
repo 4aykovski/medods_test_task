06f57cd3285a9a129d3d5f33d6e258761e887400
4aykovski <kan89kan@gmail.com>
2024-02-27 03:39:30 +0300

    feat: bcrypt hasher

--- pkg/lib/hasher/bcrypt.go
+++ pkg/lib/hasher/bcrypt.go
@@ -1,1 +1,34 @@
 package hasher
+
+import (
+	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+type BcryptHasher struct {
+}
+
+func NewBcryptHasher() *BcryptHasher {
+	return &BcryptHasher{}
+}
+
+func (b *BcryptHasher) Hash(input string) (string, error) {
+	const op = "pkg.lib.hasher.bcrypt.Hash"
+
+	hash, err := bcrypt.GenerateFromPassword([]byte(input), 12)
+	if err != nil {
+		return "", fmt.Errorf("%s: %w", op, err)
+	}
+
+	return string(hash), nil
+}
+
+func (b *BcryptHasher) CompareHash(hash string, input string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(input))
+	if err != nil {
+		return false
+	}
+
+	return true
+}
